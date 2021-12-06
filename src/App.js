6dc85b68a816fffb697d0da78adb75df7e226f3e
kirstenrc <86810764+kirstenrc@users.noreply.github.com>
2021-12-06 07:47:28 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+        <div>
+          <a
+            href="https://github.com/kirstenrc/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>
+          <span> by Kirsten Cox</span>
+        </div>
+      </div>
     </div>
   );
 }
